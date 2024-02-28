019fc72af61968d63819b553430b4c451c17ae62
HarryR-P <har1@iastate.edu>
2024-02-27 22:19:25 -0600

    added protected route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Secure from './secure'
 import Login from './login'
+import PrivateRoutes from './utils/PrivateRoutes'
 import { useEffect, useState } from 'react'
 import './App.css';
 
 function App() {
   const [email, setEmail] = useState('')
 
+
   return (
     <div className="App">
        <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login setEmail={setEmail} />} />
-          <Route path="/secure" element={<Secure email={email} />} />
+          <Route element={<PrivateRoutes />} >
+            <Route path="/secure" element={<Secure email={email} />} />
+          </Route>
+          <Route path="/login" element={<Login setEmail={setEmail} />} />
         </Routes>
       </BrowserRouter>
     </div>
